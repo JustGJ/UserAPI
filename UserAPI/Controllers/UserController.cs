24432867754e1e5388783124ba20850acd23b02c
Jeff <jf.gasp@gmail.com>
2023-07-16 15:39:47 +0200

    first commit

---
+++ UserAPI/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+       public UserController()
+       {
+       }
+
+        [HttpGet]
+        public string[] GetUsers()
+        {
+            return new string[] { "User1, user2" };
+        }
+    }
+}
