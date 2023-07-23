6243048fe76fac398cac28f1e8cf40531ffd0f71
Jeff <jf.gasp@gmail.com>
2023-07-23 22:16:00 +0200

    add controller CRUD

--- UserAPI/Controllers/UserController.cs
+++ UserAPI/Controllers/UserController.cs
@@ -2,6 +2,8 @@
 using Microsoft.Data.SqlClient;
 using System.Data;
 using UserAPI.Data;
+using UserAPI.Dtos;
+using UserAPI.models;
 
 namespace UserAPI.Controllers
 {
@@ -23,10 +25,100 @@ namespace UserAPI.Controllers
             return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
         }
 
-       [HttpGet("GetUsers/{testValue}")]
-       public string[] GetUsers(string testValue)
+       [HttpGet("GetUsers")]
+       public IEnumerable<User> GetUsers()
         {
-            return new string[] { "user1, user2", testValue };
+            string sql = @"
+            SELECT [UserId],
+            [FirstName],
+            [LastName],
+            [Email],
+            [Gender],
+            [Active] FROM TutorialAppSchema.Users";
+
+            IEnumerable<User> users = _dapper.LoadData<User>(sql);
+            return users;
+        }
+        [HttpGet("GetUserSingle/{userId}")]
+        public User GetUserSingle(int userId)
+        {
+            string sql = @"
+            SELECT [UserId],
+            [FirstName],
+            [LastName],
+            [Email],
+            [Gender],
+            [Active] FROM TutorialAppSchema.Users WHERE UserId = " + userId.ToString();
+
+            User user = _dapper.LoadDataSingle<User>(sql);
+            return user;
+        }
+
+        [HttpPut("EditUser")]
+        public IActionResult EditUser(User user)
+        {
+            string sql = @"
+        UPDATE TutorialAppSchema.Users
+            SET [FirstName] = '" + user.FirstName +
+                    "', [LastName] = '" + user.LastName +
+                    "', [Email] = '" + user.Email +
+                    "', [Gender] = '" + user.Gender +
+                    "', [Active] = '" + user.Active +
+                "' WHERE UserId = " + user.UserId;
+
+            Console.WriteLine(sql);
+
+            if (_dapper.ExecuteSql(sql))
+            {
+                return Ok();
+            }
+
+            throw new Exception("Failed to Update User");
+        }
+
+
+        [HttpPost("AddUser")]
+        public IActionResult AddUser(UserToAddDto user)
+        {
+            string sql = @"
+            INSERT INTO TutorialAppSchema.Users(
+                [FirstName],
+                [LastName],
+                [Email],
+                [Gender],
+                [Active]
+            ) VALUES (" +
+                    "'" + user.FirstName +
+                    "', '" + user.LastName +
+                    "', '" + user.Email +
+                    "', '" + user.Gender +
+                    "', '" + user.Active +
+                "')";
+
+            Console.WriteLine(sql);
+
+            if (_dapper.ExecuteSql(sql))
+            {
+                return Ok();
+            }
+
+            throw new Exception("Failed to Add User");
+        }
+        [HttpDelete("DeleteUser/{userId}")]
+        public IActionResult DeleteUser(int userId)
+        {
+            string sql = @"
+            DELETE FROM TutorialAppSchema.Users 
+                WHERE UserId = " + userId.ToString();
+
+            Console.WriteLine(sql);
+
+            if (_dapper.ExecuteSql(sql))
+            {
+                return Ok();
+            }
+
+            throw new Exception("Failed to Delete User");
         }
     }
 }
