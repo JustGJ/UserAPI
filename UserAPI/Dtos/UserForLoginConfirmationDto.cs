7f40f19ccf577bcf2736b9ef7a8ed9a86c9ed234
Jeff <jf.gasp@gmail.com>
2023-07-31 16:16:01 +0200

    add Auth and login

---
+++ UserAPI/Dtos/UserForLoginConfirmationDto.cs
@@ -0,0 +1,20 @@
+﻿namespace UserAPI.Dtos
+{
+    public partial class UserForLoginConfirmationDto
+    {
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+        public UserForLoginConfirmationDto()
+        {
+            if (PasswordHash == null)
+            {
+                PasswordHash = Array.Empty<byte>();
+            }
+            if (PasswordSalt == null)
+            {
+                PasswordSalt = Array.Empty<byte>();
+            }
+        }
+    }
+}
